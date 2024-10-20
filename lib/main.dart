2f568926e46f413a4b3b64877e2f91151b624970
Admin <madenumida@gmail.com>
2024-10-21 03:20:09 +0500

    menu_project

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'Poppins',
+        fontFamily: 'Inter',
       ),
       home: HomePage(),
     );
